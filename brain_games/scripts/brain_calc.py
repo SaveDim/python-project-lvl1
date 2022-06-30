a7d7b0ca0ab284c71c29a4715628e1f7925ab444
SaveDim <jieto35@yandex.ru>
2022-06-30 13:37:58 +0300

    Add little fixes

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -6,13 +6,11 @@
 
 from brain_games.even_logic import even_game
 
+
 def main():
     """Even game code."""
     even_game()
 
+
 if __name__ == '__main__':
     main()
-
-
-
-			CHANGE THIS CODE DIM
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,10 +6,11 @@
 
 from brain_games.even_logic import even_game
 
+
 def main():
     """Even game code."""
     even_game()
 
+
 if __name__ == '__main__':
     main()
-
