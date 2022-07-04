ec7554eb191b8500a9bdfa2ba874434ff9dd418f
SaveDim <jieto35@yandex.ru>
2022-07-04 18:51:15 +0300

    Add brain-progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,16 @@
+# -*- coding example -*-.
+
+"""Add greeting to the game."""
+
+#!/usr/bin/env python
+
+from brain_games.games.games_logic import find_skipped_number
+
+
+def main():
+    """Calc game code."""
+    find_skipped_number()
+
+
+if __name__ == '__main__':
+    main()
