be8a2fb49723d34fb13bbc9fd57e5718b054b40d
SaveDim <jieto35@yandex.ru>
2022-07-04 19:38:00 +0300

    Add prime game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,16 @@
+# -*- coding example -*-.
+
+"""Add prime game."""
+
+#!/usr/bin/env python
+
+from brain_games.games.games_logic import is_prime
+
+
+def main():
+    """Prime game code."""
+    is_prime()
+
+
+if __name__ == '__main__':
+    main()
