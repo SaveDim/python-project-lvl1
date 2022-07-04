e0aee2631a13e222fa83c54645ed764c745dc6d3
SaveDim <jieto35@yandex.ru>
2022-07-04 17:16:59 +0300

    Add brain-gcd game

--- brain_games/games/games_logic.py
+++ brain_games/games/games_logic.py
@@ -70,4 +70,29 @@ def calc_game():
             break
     if counter == 3:
         print(f"Congratulations, {name}!")
-calc_game()
+
+
+def find_gcd():
+    name = welcome_user()
+    counter = 0
+    print('Find the greatest common divisor of given numbers.')
+    for _ in range(3):
+        num_1 = random.randint(1, 101)
+        num_2 = random.randint(1, 101)
+        if num_1 > num_2:
+            num_1, num_2 = num_2, num_1
+        elif num_1 == num_2:
+            num_2 += 1
+        correct_answer = num_2
+        print(f'Question: {num_1} {num_2}')
+        while correct_answer > 0:
+            if num_1 % correct_answer == 0 and num_2 % correct_answer == 0:
+                break
+            correct_answer -= 1
+        answer = prompt.string('Your answer: ')
+        get_result(answer, correct_answer, name)
+        if str(answer) != str(correct_answer):
+            break
+        counter += 1
+    if counter == 3:
+        print(f"Congratulations, {name}!")
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,16 @@
+# -*- coding example -*-.
+
+"""Add greeting to the game."""
+
+#!/usr/bin/env python
+
+from brain_games.games.games_logic import find_gcd
+
+
+def main():
+    """Calc game code."""
+    find_gcd()
+
+
+if __name__ == '__main__':
+    main()
