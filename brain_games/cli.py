b6c5ea21fff91951f7d4afb8a84a8fc530ac0287
SaveDim <jieto35@yandex.ru>
2022-08-31 11:46:26 +0300

    Fix bugs

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -14,5 +14,5 @@ def get_user_name():
 def welcome_user():
     """Welcome func"""
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    return print(f'Hi, {name}')
+    name = prompt.string('May I have your name? \n')
+    return print(f'Hello, {name}')
