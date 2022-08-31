d9e807cf365288a98f5b8db819fe8dee5bd82683
SaveDim <jieto35@yandex.ru>
2022-08-31 12:10:49 +0300

    Fix linter errors

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -6,13 +6,13 @@ import prompt
 
 
 def get_user_name():
-    """Prompt user for his/her name."""
+    """Returns question and username."""
     print('Welcome to the Brain Games!')
     return prompt.string('May I have your name? ')
 
 
 def welcome_user():
-    """Welcome func"""
+    """Return greeting."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? \n')
     return print(f'Hello, {name}')
