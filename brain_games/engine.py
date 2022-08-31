f5de9d215aa7af05bdfdca0b864886a71c126cc9
SaveDim <jieto35@yandex.ru>
2022-08-31 10:01:10 +0300

    Change logic

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,53 +2,58 @@ from random import randint
 
 from brain_games.cli import get_user_answer, get_user_name
 
-NUMBER_OF_ROUNDS = 3
+ROUNDS_COUNT = 3
 
 
-def generate_number():
-    """Return random number from range."""
-    return randint(1, 100)
+# def generate_number():
+#     """Return random number from range."""
+#     return randint(1, 100)
+#
+#
+# def check_answer(user_answer, correct_answer):
+#     """Check users answer."""
+#     if user_answer == correct_answer:
+#         message = 'Correct!'
+#         return (True, message)
+#     message = "'{wrong}' is wrong answer ;(. Correct answer was '{correct}'."
+#     return (False, message.format(wrong=user_answer, correct=correct_answer))
+#
+#
+# def welcome_user():
+#     """Ask user for a name and print greeting."""
+#     user_name = get_user_name()
+#     greeting = f'Hello, {user_name}!'
+#     print(greeting)
+#     return user_name
+#
+#
+# def run(game=None):
+#     """Run game."""
+#     print('Welcome to the Brain Games!')
+#     if game:
+#         print(game.DESCRIPTION)
+#     print()
+#     user_name = welcome_user()
+#     if game:
+#         print()
+#         engine(user_name, game.make_question)
 
 
-def check_answer(user_answer, correct_answer):
-    """Check users answer."""
-    if user_answer == correct_answer:
-        message = 'Correct!'
-        return (True, message)
-    message = "'{wrong}' is wrong answer ;(. Correct answer was '{correct}'."
-    return (False, message.format(wrong=user_answer, correct=correct_answer))
-
-
-def welcome_user():
-    """Ask user for a name and print greeting."""
-    user_name = get_user_name()
-    greeting = f'Hello, {user_name}!'
-    print(greeting)
-    return user_name
-
-
-def run(game=None):
-    """Run game."""
-    print('Welcome to the Brain Games!')
-    if game:
-        print(game.DESCRIPTION)
-    print()
-    user_name = welcome_user()
-    if game:
-        print()
-        engine(user_name, game.make_question)
-
-
-def engine(user_name, play):
-    """Game engine process."""
-    correct_answers = 0
-    while correct_answers < NUMBER_OF_ROUNDS:
-        question, correct_answer = play()
-        print(question)
-        res, msg = check_answer(get_user_answer(), correct_answer)
-        print(msg)
-        if not res:
-            print(f"Let's try again, {user_name}!")
-            return
-        correct_answers += 1
-    print(f'Congratulations, {user_name}!')
+def engine(game):
+    """Game process."""
+    name = string('May I have your name? ')
+    (question, answer) = game.get_round()
+    print(f'Hello, {name}!')
+    print(f'{game.DESCRIPTION}')
+    for _ in ROUNDS_COUNT:
+        print(f'Question: {question}')
+        answer = string('Your answer:  ')
+        if answer == correct_answer:
+            print('Correct!')
+            (question, correct_answer) = game.get_round()
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'\n"
+                  f"Let's try again, {name}!")
+            break
+    print(f'Congratulations, {name}!')
--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,33 +1,21 @@
-import operator
-from random import choice
-
-from brain_games.engine import generate_number
+from random import choice, randint
 
 DESCRIPTION = 'What is the result of the expression?'
 
 
-operations = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-}
-
-
-def generate_operation():
-    """Generate random operation."""
-    return choice(list(operations.keys()))
-
-
-def correct_answer(num1, operation, num2):
-    """Return correct answer."""
-    return str(operations[operation](num1, num2))
-
-
-def make_question():
+def get_round():
     """Generate game question."""
-    num1 = generate_number()
-    num2 = generate_number()
-    operation = generate_operation()
-    question = f'Question: {num1} {operation} {num2}'
-    answer = correct_answer(num1, operation, num2)
-    return (question, answer)
+    num1 = randint(1, 101)
+    num2 = randint(1, 101)
+
+    correct_answers = {
+        '+': num_1 + num_2,
+        '-': num_1 - num_2,
+        '*': num_1 * num_2
+        }
+
+    random_key = choice(list(correct_answers.keys()))
+    correct_answer = correct_answers[random_key]
+    question = f'Question: {num1} {random_key} {num2}'
+
+    return question, correct_answer
