6211a7baa201e76a960ce62f451ccf0a90952712
DS <jieto35@yandex.ru>
2023-02-09 12:51:22 +0300

    Fix text of the game task

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,6 @@
 from random import randint
 
-DESCRIPTION = 'Answer "yes" if the number is even otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_round():
